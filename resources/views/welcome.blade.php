d5f03b8716f9bf22d7e471140d34464864622000
Mariko <maripiyok@gmail.com>
2015-02-27 22:50:10 +0900

    ログインページの編集。
    Boootstrapを追加してみた。
    Viewコードの書き換え方を練習中。

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,8 @@
 <html>
 	<head>
 		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
 
 		<style>
 			body {
@@ -13,33 +15,14 @@
 				font-weight: 100;
 				font-family: 'Lato';
 			}
-
-			.container {
-				text-align: center;
-				display: table-cell;
-				vertical-align: middle;
-			}
-
-			.content {
-				text-align: center;
-				display: inline-block;
-			}
-
-			.title {
-				font-size: 96px;
-				margin-bottom: 40px;
-			}
-
-			.quote {
-				font-size: 24px;
-			}
 		</style>
 	</head>
+
 	<body>
 		<div class="container">
-			<div class="content">
-				<div class="title">Laravel 5</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
+			<div class="jumbotron">
+				<h1>Lala Ballet</h1>
+				<p>{{ Inspiring::quote() }}</p>
 			</div>
 		</div>
 	</body>
