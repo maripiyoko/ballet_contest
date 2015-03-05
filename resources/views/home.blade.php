cd53f7aa17aec2d6a8254230cd99f958fc86274c
Mariko <maripiyok@gmail.com>
2015-03-06 07:25:04 +0900

    #2 ホーム画面の項目修正。

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,10 @@
 @section('content')
 <div class="container">
 	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">{{ $organization->name }}</div>
-				<div class="panel-body">
-          Hello! {{ $currentUser->name }}
-				</div>
+		<div class="col-md-10">
+      <h1>{{ $organization->name }}</h1>
+			<div class="row col-md-12">
+        Select Contest.
 			</div>
 		</div>
 	</div>
