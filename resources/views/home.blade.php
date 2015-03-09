d8726b4c7df4ffcc9b6989b5a791f9eb47d21d72
Mariko <maripiyok@gmail.com>
2015-03-10 07:42:48 +0900

    ホーム画面から組織の持っているコンテストを選択できるようにした。

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,21 @@
 		<div class="col-md-10">
       <h1>{{ $organization->name }}</h1>
 			<div class="row col-md-12">
-        Select Contest.
+        <form class="form-horizontal">
+          <div class="form-group">
+            <label class="col-sm-3 control-label">コンテストを選んでください</label>
+            <div class="col-sm-7">
+              <select class="form-control">
+                @foreach( $organization->contests as $contest )
+                  <option value="{{ $contest->id }}">{{ $contest->name }}</option>
+                @endforeach
+              </select>
+            </div>
+            <div class="col-sm-2">
+              <button class="btn btn-primary" type="submit">スコアを入力する</button>
+            </div>
+          </div>
+        </form>
 			</div>
 		</div>
 	</div>
