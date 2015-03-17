3136c6cd88636b202c3fbba1938ee491cd62b38d
Mariko <maripiyok@gmail.com>
2015-03-18 07:19:22 +0900

    scoreテーブルにカラムを追加。

--- app/Score.php
+++ app/Score.php
@@ -4,6 +4,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Score extends Model {
 
-	//
+  protected $table = 'scores';
+
+  public function viewpoint() {
+    return $this->belongsTo('App\ViewPoint');
+  }
+
+  public function judge() {
+    return $this->belongsTo('App\Judge');
+  }
+
+  public function user() {
+    return $this->belongsTo('App\User');
+  }
+
+  public function player() {
+    return $this->belongsTo('App\Player');
+  }
 
 }
--- database/migrations/2015_03_18_070628_create_scores_table.php
+++ database/migrations/2015_03_18_070628_create_scores_table.php
@@ -15,6 +15,19 @@ class CreateScoresTable extends Migration {
 		Schema::create('scores', function(Blueprint $table)
 		{
 			$table->increments('id');
+      $table->integer('score')->unsigned();
+      $table->integer('player_id')->unsigned()->default(0);
+      $table->foreign('player_id')
+        ->references('id')->on('players')->onDelete('cascade');
+      $table->integer('judge_id')->unsigned()->default(0);
+      $table->foreign('judge_id')
+        ->references('id')->on('judges')->onDelete('cascade');
+      $table->integer('viewpoint_id')->unsigned()->default(0);
+      $table->foreign('viewpoint_id')
+        ->references('id')->on('viewpoints')->onDelete('cascade');
+      $table->integer('user_id')->unsigned()->default(0);
+      $table->foreign('user_id')
+        ->references('id')->on('users')->onDelete('cascade');
 			$table->timestamps();
 		});
 	}
