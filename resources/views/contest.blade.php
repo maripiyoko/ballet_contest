be9cc69eac2e5a0314c4a304ef452402069291a3
Mariko <maripiyok@gmail.com>
2015-03-19 07:48:25 +0900

    #22 コンテストから部門を選ぶところの、画面スタイルを調整。
    ダミーで入力件数を表示するバッヂを置いてみた。

--- resources/views/contest.blade.php
+++ resources/views/contest.blade.php
@@ -5,14 +5,21 @@
 	<div class="row">
 		<div class="col-md-10">
       <h1>{{ $contest->name }}</h1>
-			<div class="row col-md-12">
-        <ul>
-          @foreach( $contest->groups as $group )
-            <li>
-              <a href="/group/{{ $group->id }}">{{ $group->name }}</a>
-            </li>
-          @endforeach
-        </ul>
+			<div class="row col-md-6">
+        <div class="panel panel-default">
+          <div class="panel-heading">スコアを入力する部門を選んでください</div>
+          <div class="panel-body">
+            <ul class="list-group">
+              @foreach( $contest->groups as $group )
+                <a href="/group/{{ $group->id }}">
+                  <li class="list-group-item">
+                    <span class="badge">0</span>
+                    <span>{{ $group->name }}</span>
+                  </li>
+                </a>
+              @endforeach
+            </ul>
+          </div>
       </div>
 		</div>
 	</div>
