327b1d5e19fcb2eb342b9a11e7816bc813ff1f9c
Mariko <maripiyok@gmail.com>
2015-03-19 07:31:31 +0900

    #22 ヘッダー部分の組織名で、組織ホームに戻れるようにする

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -28,7 +28,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="/">
           {{ isset($organization) ? $organization->name : 'Lala Ballet' }}
         </a>
 			</div>
