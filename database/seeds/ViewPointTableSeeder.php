307eee25f6ac2cdc3f25f49ec415b41d61eb60b3
mayumiH <kongoujimayumi@gmail.com>
2015-03-21 11:37:51 +0900

    観点の内容を追加 Fixed #23

--- database/seeds/ViewPointTableSeeder.php
+++ database/seeds/ViewPointTableSeeder.php
@@ -13,20 +13,44 @@ class ViewPointTableSeeder extends Seeder {
     $vp_array = [
       [
         'id' => 1,
-        'name' => '演技力',
-        'description' => '演技力があるか',
+        'name' => '芸術性',
+        'description' => '芸術性の説明',
         'contest_id' => 1
       ],
       [
         'id' => 2,
-        'name' => '音楽',
-        'description' => '音楽と合っているか',
+        'name' => '技術',
+        'description' => '技術の説明',
         'contest_id' => 1
       ],
       [
         'id' => 3,
-        'name' => '構成',
-        'description' => '全体の構成バランス',
+        'name' => '感性',
+        'description' => '感性の説明',
+        'contest_id' => 1
+      ],
+      [
+        'id' => 4,
+        'name' => '音楽',
+        'description' => '音楽の説明',
+        'contest_id' => 1
+      ],
+      [
+        'id' => 5,
+        'name' => '表現力',
+        'description' => '表現力の説明',
+        'contest_id' => 1
+      ],
+      [
+        'id' => 6,
+        'name' => '容姿',
+        'description' => '容姿の説明',
+        'contest_id' => 1
+      ],
+      [
+        'id' => 7,
+        'name' => '将来性',
+        'description' => '将来性の説明',
         'contest_id' => 1
       ]
     ];
