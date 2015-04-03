66dfcaa1129526457451395635a58051c2811179
Mariko <maripiyok@gmail.com>
2015-04-04 08:13:22 +0900

    結果テーブルviewをExcelダウンロード用に分割。

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -21,46 +21,7 @@
         <div class="tab-content">
           @foreach( $contest->groups as $group )
           <div role="tabpanel" class="tab-pane fade" id="group-{{ $group->id }}">
-          <table class="table table-hover table-bordered">
-          <thead>
-            <tr>
-              <th>観点</th>
-              @foreach ( $contest->viewpoints as $viewpoint )
-                <th colspan="{{ count($viewpoint->judges()) }}">{{ $viewpoint->name }}</th>
-              @endforeach
-            </tr>
-            <tr>
-              <th>審査員</th>
-              @foreach ( $contest->viewpoints as $viewpoint )
-                @foreach ( $viewpoint->judges() as $j )
-                <th>{{ $j->name }}</th>
-                @endforeach
-              @endforeach
-            </tr>
-          </thead>
-          @foreach ( $group->players as $player )
-            <tr>
-              <th rowspan="2">{{ $player->name }}</th>
-              @foreach( $contest->viewpoints as $viewpoint)
-                @foreach ( $viewpoint->judges() as $j )
-                <td class="text-right">
-	                {{-- */$score = $contest->score($j->id, $viewpoint->id, $player->id) /* --}}
-                  @if( isset($score) )
-                  {{ $score->score }}
-                  @endif
-                </td>
-                @endforeach
-              @endforeach
-            </tr>
-            <tr>
-              @foreach( $contest->viewpoints as $viewpoint)
-              <td class="text-right" colspan="{{ count($viewpoint->judges()) }}">
-                {{ intval($viewpoint->sumupScores($player->id)) }}
-              </td>
-              @endforeach
-            </tr>
-          @endforeach
-          </table>
+            @include('result-table', array('contest', $contest) )
           </div>
           @endforeach
         </div>
