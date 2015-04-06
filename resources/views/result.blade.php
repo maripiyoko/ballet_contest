012a9f3dc01cd7e38feb7cb7b34010f634fe5560
Mariko <maripiyok@gmail.com>
2015-04-07 07:43:18 +0900

    結果表示のレイアウト修正

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -3,11 +3,18 @@
 @section('content')
 <div class="container">
 	<div class="row">
-
 		<div class="col-md-12">
-      <h1>{{ $contest->name }} 結果</h1>
-			<div class="row col-md-12">
 
+      <div class="row col-md-12">
+        <h2>
+          <a href="/contest/{{ $contest->id }}">{{ $contest->name }}</a> 結果
+          <a href="/result/download/{{ $contest->id }}" class="btn btn-default pull-right">
+            <span>Excelダウンロード</span>
+          </a>
+        </h2>
+      </div>
+
+			<div class="row col-md-12">
         <div role="tabpanel">
           <ul class="nav nav-tabs" role="tablist">
             @foreach( $contest->groups as $group )
